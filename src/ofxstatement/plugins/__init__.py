d88a7897406c7c9e130fd56ffa8cd57d4088eb62
bubbas <seca2@freenet.de>
2013-02-07 17:07:14 +0100

    uncommented accidental commented plugins __init__.py

--- src/ofxstatement/plugins/__init__.py
+++ src/ofxstatement/plugins/__init__.py
@@ -1,10 +1,10 @@
 """Import all plugins for them to autoregister
 """
 
-#from . import litas_esis
-#from . import swedbank
-#from . import maxibps
-#from . import danske
-#from . import dkb_cc
-#from . import lbbamazon
+from . import litas_esis
+from . import swedbank
+from . import maxibps
+from . import danske
+from . import dkb_cc
+from . import lbbamazon
 from . import bos_de
